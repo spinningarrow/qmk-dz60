e537ff7ad26463bbea9eef2385c9e2f43fbbac4f
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-06-13 10:30:13 +0800

    Tap dance left Cmd to Meh

--- keymap.c
+++ keymap.c
@@ -8,7 +8,7 @@
 #define F_KEYS 2
 
 enum {
-  TD_GUI_LCAG = 0
+  TD_GUI_MEH = 0
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TAB,        KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
        CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,
        KC_LSPO,       ______,  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC, KC_UP,   TT(F_KEYS),
-       KC_MEH,       KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+       KC_MEH,       KC_LALT,  TD(TD_GUI_MEH), KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   [F_APPLE] = LAYOUT_2_shifts(
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_GUI_LCAG] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_HYPR)
+  [TD_GUI_MEH] = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, KC_MEH)
 };
 
 enum function_id {
