988b53ea1b41bcdab615bf9d47889e861f226fa0
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-11-10 17:54:33 +0100

    Add page up and page down keys
    - Fixed layout name
    - Moved Reset key

--- keymap.c
+++ keymap.c
@@ -12,28 +12,28 @@ enum {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_2_shifts(
-       KC_GRV,        KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC, TT(F_APPLE),
-       KC_TAB,        KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
-       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,
-       KC_LSPO,       ______,  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC, KC_UP,   TT(F_KEYS),
-       KC_MEH,       KC_LALT,  KC_LGUI, ______, KC_SPC, ______, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+  [BASE] = LAYOUT_all(
+       KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, TT(F_APPLE),
+       KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+       CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+       KC_LSPO,       ______,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   TT(F_KEYS),
+       KC_MEH,        KC_LALT, KC_LGUI, ______,  KC_SPC,  ______, KC_RGUI, KC_RALT,   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
-  [F_APPLE] = LAYOUT_2_shifts(
-      ______, KC_F14, KC_F15, ______, ______, ______, ______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, ______, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,  ______,
-      ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,
-      ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______,  ______, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______
+  [F_APPLE] = LAYOUT_all(
+      ______,         KC_F14,  KC_F15,  ______,  ______,  ______, ______,  KC_MPRV,   KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  KC_PGDOWN, KC_PGUP, ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______
   ),
 
-  [F_KEYS] = LAYOUT_2_shifts(
-      KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,  ______,  ______,
-      ______, RGB_MOD, ______,  ______,  RESET,   ______, ______, ______, ______, ______, ______, RGB_VAI, RGB_VAD, RGB_TOG,
-      ______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, ______, ______, ______, ______, ______, ______, ______,  ______,
-      ______, ______,  ______,  ______,  ______,  ______, ______, ______, ______, ______, ______, ______,  ______,  ______,  ______,
-      ______, ______,  ______,  ______,  ______,  ______, ______, ______, ______, ______, ______
+  [F_KEYS] = LAYOUT_all(
+      ______,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ______,  RESET,
+      ______,         RGB_MOD, ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  RGB_VAI, RGB_VAD, RGB_TOG,
+      ______,         RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______
   ),
 };
 
