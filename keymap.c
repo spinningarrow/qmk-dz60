6631ccf2742d2bd854884608fd6871474507bbe2
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2020-08-13 12:33:53 +0200

    Try to have some arrow keys in vim cluster

--- keymap.c
+++ keymap.c
@@ -17,13 +17,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
        CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
        KC_LSPO,       ______,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   TT(F_KEYS),
-       KC_MEH,        KC_LALT, KC_LGUI, ______,  KC_SPC,  ______, KC_RGUI, KC_RALT,   KC_LEFT, KC_DOWN, KC_RIGHT
+       TT(F_APPLE),        KC_LALT, KC_LGUI, ______,  KC_SPC,  ______, KC_RGUI, KC_RALT,   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
   [F_APPLE] = LAYOUT_all(
       ______,         KC_F14,  KC_F15,  ______,  ______,  ______, ______,  KC_MPRV,   KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, ______,  ______,
       ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,  ______,
-      ______,         ______,  ______,  ______,  ______,  ______, ______,  KC_PGDOWN, KC_PGUP, ______,  ______,  ______,  ______,
+      ______,         ______,  ______,  ______,  ______,  ______, KC_LEFT,  KC_DOWN, KC_UP, KC_RIGHT,  ______,  ______,  ______,
       ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______,  ______,  ______,  ______,  ______,
       ______,         ______,  ______,  ______,  ______,  ______, ______,  ______,    ______,  ______,  ______
   ),
